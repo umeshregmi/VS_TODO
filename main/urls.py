2b0f85be0fb49e6309ed9e5ccfc60c7b3dc06102
umeshregmi <regmiumesh51@gmail.com>
2024-08-13 08:06:08 +0545

    crud

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from main import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('add/', views.add_todo, name='add-todo')
+    path('add/', views.add_todo, name='add-todo'),
+    path('delete/<int:id>', views.delete, name='delete-todo'),
+    path('edit/<int:id>', views.edit, name='edit-todo')
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.models import TODO
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/home.html')
+    todos = TODO.objects.all()
+    return render(request, 'main/home.html', {'todos': todos})
 
 def add_todo(request):
-    return render(request, 'add.html')
+    if request.method == 'GET':
+        return render(request, 'main/add.html')
+    else:
+        title = request.POST['title']
+        content = request.POST['content']
+        try:
+            TODO.objects.create(title=title, content=content, user_id=request.user.id, is_completed=False)
+            return redirect('home')
+        except Exception as e:
+            return redirect('add-todo')
+
+def delete(request, id):
+    todo = TODO.objects.get(id=id)
+    todo.delete()
+    return redirect('home')
+
+def edit(request, id):
+    todo = TODO.objects.get(id=id)
+
+    if request.method == 'GET':
+        return render(request, 'main/edit.html', context={'todo': todo})
+    else:
+        todo.title  = request.POST['title']
+        todo.content = request.POST['content']
+
+        todo.save()
+
+        return redirect('home')
